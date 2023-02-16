c378f70898a316ae2a4d1ef471329327452dbf84
Jarkynbek <44564085+Jarkynbekbat@users.noreply.github.com>
2023-02-16 23:34:05 +0600

    remove old src

--- old/lib/helpers/show_snackbar.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-void showSnackBar(message, _scaffoldKey) {
-  final snackBar = SnackBar(content: Text(message));
-  _scaffoldKey.currentState.showSnackBar(snackBar);
-}
