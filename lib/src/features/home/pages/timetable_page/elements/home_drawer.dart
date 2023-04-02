d8b31472d96f94968ce9890521c8c74adc50e5d8
Nurzhigit <106031957+Nurzhigit97@users.noreply.github.com>
2023-04-02 22:45:53 +0600

    new: toggle theme(position theme icon)

--- lib/src/features/home/pages/timetable_page/elements/home_drawer.dart
+++ lib/src/features/home/pages/timetable_page/elements/home_drawer.dart
@@ -12,41 +12,53 @@ class HomeDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          AppDrawerMenuItem(
-            onTap: () {},
-            leading: const Icon(
-              Icons.school_outlined,
-              color: AppColors.primaryColor,
-            ),
-            title: 'График сессий',
-          ).dev(),
-          AppDrawerMenuItem(
-            onTap: () {},
-            leading: const Icon(
-              Icons.school_outlined,
-              color: AppColors.primaryColor,
-            ),
-            title: 'График модулей',
-            isLast: true,
-          ).dev(),
-          const SizedBox(height: 40.0),
-          AppDrawerMenuItem(
-            onTap: () {
-              Scaffold.of(context).openEndDrawer();
-              Navigator.of(context).pushNamed('/about');
-            },
-            leading: Assets.icLauncher.image(
-              width: 32.0,
-              height: 32.0,
-            ),
-            title: 'О приложении',
-            isLast: true,
+        child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Positioned(
+          top: 20,
+          right: 20,
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.dark_mode),
           ),
-        ],
-      ),
-    );
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AppDrawerMenuItem(
+              onTap: () {},
+              leading: const Icon(
+                Icons.school_outlined,
+                color: AppColors.primaryColor,
+              ),
+              title: 'График сессий',
+            ).dev(),
+            AppDrawerMenuItem(
+              onTap: () {},
+              leading: const Icon(
+                Icons.school_outlined,
+                color: AppColors.primaryColor,
+              ),
+              title: 'График модулей',
+              isLast: true,
+            ).dev(),
+            const SizedBox(height: 40.0),
+            AppDrawerMenuItem(
+              onTap: () {
+                Scaffold.of(context).openEndDrawer();
+                Navigator.of(context).pushNamed('/about');
+              },
+              leading: Assets.icLauncher.image(
+                width: 32.0,
+                height: 32.0,
+              ),
+              title: 'О приложении',
+              isLast: true,
+            ),
+          ],
+        )
+      ],
+    ));
   }
 }
