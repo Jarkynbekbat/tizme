24da2eb0a844bfc7b928a6af93d6efd0b552f00e
Nurzhigit <106031957+Nurzhigit97@users.noreply.github.com>
2023-04-05 08:27:35 +0600

    new: theme cache

--- lib/src/shared/data/repos/app_cache_repo.dart
+++ lib/src/shared/data/repos/app_cache_repo.dart
@@ -3,6 +3,7 @@ import 'package:studtime/src/shared/data/cache/chat_cache.dart';
 import 'package:studtime/src/shared/data/cache/is_first_launch_cache.dart';
 import 'package:studtime/src/shared/data/cache/is_intro_shown_cache.dart';
 import 'package:studtime/src/shared/data/cache/setup_cache.dart';
+import 'package:studtime/src/shared/data/cache/theme_cache.dart';
 
 /// Репозиторий для работы с кэшем
 class AppCacheRepo {
@@ -10,6 +11,7 @@ class AppCacheRepo {
   late final SetupCache settingsCache;
   late final IsIntroShownCache isIntroShownCache;
   late final ChatCache chatCache;
+  late final ThemeCache themeCache;
 
   /// Инициализируем все кэши
   Future<void> init() async {
@@ -18,5 +20,6 @@ class AppCacheRepo {
     settingsCache = SetupCache(prefs, 'settings');
     isIntroShownCache = IsIntroShownCache(prefs, 'is_intro_shown');
     chatCache = ChatCache(prefs, 'chat');
+    themeCache = ThemeCache(prefs, 'isDarkMode');
   }
 }
